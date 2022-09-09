81305b9b4231d59b785c848e02493956895e12ac
Ibrar <naseer.ibrar@gmail.com>
2022-09-09 21:15:19 +0200

    Add logic for winning. Start logic for multiple rounds.

--- myScript.js
+++ myScript.js
@@ -1,5 +1,34 @@
 let computerChoice = ["Rock", "Paper", "Scissors"]
-function getComputerChoice () {
+function getComputerChoice() {
     return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 }
-console.log(getComputerChoice(computerChoice));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie. Try again.");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("Computer chose paper. You lost.");
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("Computer chose scissors. You won!");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("Computer chose scissors. You lost.");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("Computer chose rock. You won!");
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("Computer chose rock. You lost.");
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("Computer chose paper. You won!");
+    }
+}
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        playRound();
+    }
+}
+
+const playerInput = prompt("Pick your weapon", "Rock");
+const playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
+const computerSelection = getComputerChoice();
+
+console.log(game());
