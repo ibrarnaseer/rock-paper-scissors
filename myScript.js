bda218286cda04894e8c828df8dfa2a5289edb7a
Ibrar <naseer.ibrar@gmail.com>
2022-09-10 10:33:02 +0200

    Implement winning logic for complete round

--- myScript.js
+++ myScript.js
@@ -9,18 +9,22 @@ function playRound(playerSelection, computerSelection) {
         return result;
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         console.log("Computer chose paper. You lost.");
-        return result - 1;
+        return result = result - 1;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log("Computer chose scissors. You won!");
-        return result + 1;
+        return result = result + 1;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         console.log("Computer chose scissors. You lost.");
+        return result = result - 1;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log("Computer chose rock. You won!");
+        return result = result + 1;
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         console.log("Computer chose rock. You lost.");
+        return result = result - 1;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log("Computer chose paper. You won!");
+        return result = result + 1;
     }
 }
 
@@ -34,5 +38,14 @@ function game() {
         console.log(result);
     }
 }
+
 let result = 0;
 console.log(game());
+
+if (result == 0) {
+    console.log("It's a tie game!");
+} else if (result > 0) {
+    console.log("Congratulations. You won!");
+} else if (result < 0) {
+    console.log("You lost.");
+}
