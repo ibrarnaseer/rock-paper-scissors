59af95bd7636c680759384e9171b065916f84515
Ibrar <naseer.ibrar@gmail.com>
2022-09-10 11:21:44 +0200

    Implement tracking of score

--- myScript.js
+++ myScript.js
@@ -1,33 +1,39 @@
+// Array of choices computer can make
 let computerChoice = ["Rock", "Paper", "Scissors"]
+// Function to choose a weapon randomly from array
 function getComputerChoice() {
     return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 }
 
+// Function to play one round, taking into account what computer chose, and what the player picked
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a tie. Try again.");
-        return result;
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         console.log("Computer chose paper. You lost.");
-        return result = result - 1;
+        return computerScore = computerScore + 1;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log("Computer chose scissors. You won!");
-        return result = result + 1;
+        return playerScore = playerScore + 1;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         console.log("Computer chose scissors. You lost.");
-        return result = result - 1;
+        return computerScore = computerScore + 1;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log("Computer chose rock. You won!");
-        return result = result + 1;
+        return playerScore = playerScore + 1;
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         console.log("Computer chose rock. You lost.");
-        return result = result - 1;
+        return computerScore = computerScore + 1;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log("Computer chose paper. You won!");
-        return result = result + 1;
+        return playerScore = playerScore + 1;
+    } else if (playerSelection !== "Rock" || playerSelection !== "Paper" || playerSelection !== "Scissors") {
+        console.log("Invalid choice. Computer gets a point.");
+        return computerScore = computerScore + 1;
     }
 }
 
+// Function to play a game of five rounds
 function game() {
     for (i = 0; i < 5; i++) {
         const playerInput = prompt("Pick your weapon");
@@ -35,17 +41,22 @@ function game() {
         const computerSelection = getComputerChoice();
 
         playRound(playerSelection, computerSelection);
-        console.log(result);
+        // Keeping score of points in between rounds
+        console.log("Current score is: \nPlayer = " + playerScore + "\nComputer = " + computerScore);
     }
 }
 
-let result = 0;
+// To keep track of score for computer and player
+let computerScore = 0;
+let playerScore = 0;
+// Let player know score each round, and what computer chose that round
 console.log(game());
 
-if (result == 0) {
+// If else statement to let player know who won the GAME, not round(s)
+if (playerScore == computerScore) {
     console.log("It's a tie game!");
-} else if (result > 0) {
-    console.log("Congratulations. You won!");
-} else if (result < 0) {
-    console.log("You lost.");
+} else if (playerScore > computerScore) {
+    console.log("Congratulations. You won the game!");
+} else if (playerScore < computerScore) {
+    console.log("You lost the game.");
 }
